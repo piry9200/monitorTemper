12763ad80b8ea2db0996e31e8ed72abe2d516656
piry9200 <ymt104ro09@gmail.com>
2022-10-14 15:51:40 +0900

    センサからデータを受け取ったらcloud firestoreへデータを送るようにした（未テスト）

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import json
+import dataToDB
 
 app = Flask(__name__)
 
@@ -13,7 +14,9 @@ def hello():
 def sendDataToDB():
     param = json.loads(request.json)
     print(param)
-    dates = param["date"]
+    date = param["date"]
     temperature = param["temperature"]
     humidity = param["humidity"]
+    dataToDB(date, temperature, humidity)
     return ('200')
+
